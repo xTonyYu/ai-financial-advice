003564333b88043f50876c57f8bf33ab11b90cad
xTonyYu <xtonyyu@gmail.com>
2023-11-09 15:27:09 -0800

    update

--- components/Personas.js
+++ components/Personas.js
@@ -1,15 +1,32 @@
 import Image from 'next/image'
 import styles from '@/styles/Personas.module.css'
+import { useEffect } from 'react';
 
 export default function Personas({isMobBoss, setIsMobBoss}) {
+  useEffect(() => {
+    const selectAdvicer = () => {
+      const boss = document.querySelector(".boss")
+      const richPerson = document.querySelector(".richperson")
+
+      if (isMobBoss) {
+        boss.classList.add(`${styles.glow}`)
+        richPerson.classList.remove(`${styles.glow}`)
+      } else {
+        boss.classList.remove(`${styles.glow}`)
+        richPerson.classList.add(`${styles.glow}`)
+      }
+    };
+
+    selectAdvicer();
+  }, [isMobBoss]);
+
   return (
     <div className={styles.personas}>
-      <div className={styles.persona}>
+      <div className="${styles.persona} boss">
         <div>Mob Boss</div>
         <Image
           src="/mafia-boss.svg"
           alt="Mob boss image"
-          className={styles.boss}
           width={125}
           height={96}
           priority
@@ -23,12 +40,11 @@ export default function Personas({isMobBoss, setIsMobBoss}) {
           onChange={() => setIsMobBoss(!isMobBoss)}
         />
       </div>
-      <div className={styles.persona}>
+      <div className="${styles.persona} richperson">
         <div>Rich Person</div>
         <Image
           src="/rich-person.svg"
           alt="Rich person image"
-          className={styles.richperson}
           width={125}
           height={96}
           priority
